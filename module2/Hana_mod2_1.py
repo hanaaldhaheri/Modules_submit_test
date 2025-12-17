2f42bb3cdf33cf807a41ea96740fbed4d580da41
hanaaldhaheri <hana.aldhaheri@outlook.com>
2025-12-17 13:45:49 +0400

    update data

---
+++ module2/Hana_mod2_1.py
@@ -0,0 +1,10 @@
+def check_number_even_or_odd():
+    
+    number=int(input("Enter number:"))
+    if number %2 == 0:
+        print("even")
+    else:
+        print("odd")
+    
+check_number_even_or_odd()
+
