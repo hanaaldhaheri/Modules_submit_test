618639dbc836aa7dc7da752b5a912bd9e73c992f
hanaaldhaheri <hana.aldhaheri@outlook.com>
2025-12-17 11:51:01 +0400

    update

--- Hana_mod1_8.py
+++ Hana_mod1_8.py
@@ -3,7 +3,8 @@ def word_count ():
     char_len = 0
     with open("person_details.txt","r") as f:
         line=f.readline()
-        char_len = len(line)
+        word_len=line.split(",")
+        char_len = len(word_len)
     
     with open("person_details.txt","a") as f:
         f.write(str(char_len))
